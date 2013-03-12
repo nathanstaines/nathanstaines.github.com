5364e7ec9bf3f3107c6ff72a377aa7bb153520de
Nathan Staines <nathan@nathanstaines.com>
2013-03-12 11:38:38 +0000

    Update the like button

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,96 +1,83 @@
-var likes = $('#likes'),
-    pageId = likes.data('page-id'),
-    votes = false,
+var $likes = $('#likes'),
+    $count = $likes.find('span.count'),
+    $pageId = $likes.data('page-id'),
+    hasLikes = false,
     database = 'nathanstaines',
     collection = 'likes',
     apiKey = '5023a018e4b07b766e03f324';
 
-if (likes.length) {
+if ($likes.length) {
 
-  // Check if data is stored locally otherwise talk to the database
-  if (localStorage && localStorage.getItem(pageId)) {
-    var data = JSON.parse(localStorage.getItem(pageId));
-
-    likes.find('span.count').text(data.count);
+  if (localStorage && localStorage.getItem($pageId)) {
+    var data = JSON.parse(localStorage.getItem($pageId));
 
     if (data.counted) {
-      likes
-        .addClass('counted')
+      $likes.addClass('counted')
         .find('i')
         .toggleClass('icon-heart-empty icon-heart-full');
     }
+  }
 
+  if (sessionStorage && sessionStorage.getItem($pageId)) {
+    var data = JSON.parse(sessionStorage.getItem($pageId));
+
+    $count.text(data.count);
   } else {
-
     $.ajax({
       url: 'https://api.mongolab.com/api/1/databases/' + database + '/collections/' + collection + '?apiKey=' + apiKey,
       type: 'GET',
       contentType: 'application/json',
       success: function(data) {
-
-        // If data exists on the database get it
-        $.each(data.reverse(), function(index, value) {
-
-          // Save the data in local storage
-          localStorage.setItem(value.pageId, JSON.stringify({count: value.count, counted: false}));
-
-          if (value.pageId === pageId) {
-            likes.find('span.count').text(value.count);
-            votes = true;
-
-            // Exit .each() early
-            return false;
+        $.each(data, function(index, value) {
+          if (value.pageId === $pageId) {
+            $count.text(value.count);
+            hasLikes = true;
           }
+
+          sessionStorage.setItem(value.pageId, JSON.stringify({count: value.count}));
         });
 
-        // If data doesn't exist on the database add it
-        if (!votes) {
+        if (!hasLikes) {
           $.ajax({
             url: 'https://api.mongolab.com/api/1/databases/' + database + '/collections/' + collection + '?apiKey=' + apiKey,
             data: JSON.stringify({
-              pageId: pageId,
+              pageId: $pageId,
               count: 0
             }),
             type: 'POST',
             contentType: 'application/json',
             success: function() {
-
-              // Save the data in local storage
-              localStorage.setItem(pageId, JSON.stringify({count: value.count, counted: false}));
+              sessionStorage.setItem($pageId, JSON.stringify({count: 0}));
             }
           });
         }
       }
     });
-
   }
 
-  // Fire up the click event
-  likes.on('click', function(e) {
-    var num = likes.find('span.count').text(),
-        crement = likes.hasClass('counted') ? num-- : num++,
-        counted = likes.hasClass('counted') ? false : true;
+  $likes.on('click', function(e) {
+    var num = $count.text(),
+        counted = $likes.hasClass('counted') ? false : true;
 
-    likes
-      .toggleClass('counted')
-        .find('span.count')
-        .text(num)
-        .end()
-          .find('i')
-          .toggleClass('icon-heart-empty icon-heart-full');
+    num = $likes.hasClass('counted') ? num-- : num++;
 
-    // Update the data in local storage
-    localStorage.setItem(pageId, JSON.stringify({count: num, counted: counted}));
+    $likes.toggleClass('counted')
+      .find('i')
+      .toggleClass('icon-heart-empty icon-heart-full');
 
-    // Update the data in the database
+    $count.text(num);
+
+    sessionStorage.setItem($pageId, JSON.stringify({count: num}));
+    localStorage.setItem($pageId, JSON.stringify({counted: counted}));
+
     $.ajax({
       url: 'https://api.mongolab.com/api/1/databases/' + database + '/runCommand?apiKey=' + apiKey,
       data: JSON.stringify({
         findAndModify: collection,
         query: {
-          pageId: pageId
+          pageId: $pageId
         },
-        update: {$inc: {count: (likes.hasClass('counted') ? 1 : -1)}},
+        update: {$inc: {count: ($likes.hasClass('counted') ? 1 : -1)}},
         upsert: true
       }),
       type: 'POST',
@@ -99,4 +86,5 @@ if (likes.length) {
 
     e.preventDefault();
   });
+
 }
